cdf825ae1cbe9301c4b1a8f00f63e510cc9dbe20
Anumitharavikumar-sparkout <163837947+Anumitharavikumar-sparkout@users.noreply.github.com>
2024-04-03 18:20:10 +0530

    Update ascending.js

--- ascending.js
+++ ascending.js
@@ -1,16 +1,15 @@
 let num = [4, 2, 65, 21, 5, 1, 7];
 let tp=0;
 for (let i = 0; i < num.length; i++)
-{
- for (let j = 0; j < num.length-1; j++)
+
      {
-      if (num[j] > num[i])
+      if (num[i] > num[i+1])
          {
-             tp = num[j];
-            num[j] = num[i];
-            num[i] = tp;
+             tp = num[i];
+            num[i] = num[i+1];
+            num[i+1] = tp;
         }
     }
-}
+
 console.log('ascending order',num);
 console.log('descending order',num.reverse());
