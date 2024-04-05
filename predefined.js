c846c72f99060989535e8762734a5c42bd1386d0
Anumitharavikumar-sparkout <163837947+Anumitharavikumar-sparkout@users.noreply.github.com>
2024-04-05 09:55:18 +0530

    Add files via upload

---
+++ predefined.js
@@ -0,0 +1,9 @@
+let points = [5,7,8,6,9];
+points.sort();
+console.log(points);
+let x = points.find(() => true);
+console.log(x);
+let y = points.reverse().find(() => true);
+console.log(y);
+    
+
